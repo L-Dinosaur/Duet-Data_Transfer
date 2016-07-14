a29a752e66f28de165226d347c989b2998dd3186
Hao (Dino) Liu <L-Dinosaur@github.com>
2016-07-13 20:09:18 -0400

    added in date function

--- server-progs/server.c
+++ server-progs/server.c
@@ -1,96 +1,41 @@
-/* A simple server in the internet domain using TCP
-   The port number is passed as an argument 
-   This version runs forever, forking off a separate 
-   process for each connection
-*/
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h> 
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <dirent.h>
+#include "server.h"
 
-void dostuff(int); /* function prototype */
 void error(const char *msg)
 {
     perror(msg);
     exit(1);
 }
 
-int main(int argc, char *argv[])
+void get_date(struct date * date)
 {
+        time_t timeval;
+        struct tm * l_time;
+        char mon_char[2];
+        time(&timeval);
+        l_time = localtime(&timeval);
+        int year = l_time->tm_year + 1900, month = l_time->tm_mon + 1, day = l_time->tm_mday;
+        sprintf(date->year, "%d", year);
+        if(month < 10) {
+                sprintf(date->month, "0%d", month);
+        }
+        else
+                sprintf(date->month, "%d", month);
+        sprintf(date->day, "%d", day);
 
-
-     /* Declaration */
-	int sockfd, newsockfd, portno, pid;
-	socklen_t clilen;
-	struct sockaddr_in serv_addr, cli_addr;
-	char uuid_char[33];
-	char path[256];
-
-
-
-     	if (argc < 2) {
-		fprintf(stderr,"ERROR, no port provided\n");
-         	exit(1);
-     	}
-
-
-
-     	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     	if (sockfd < 0) 
-        	error("ERROR opening socket");
-     	bzero((char *) &serv_addr, sizeof(serv_addr));
-     	portno = atoi(argv[1]);
-     	serv_addr.sin_family = AF_INET;
-     	serv_addr.sin_addr.s_addr = INADDR_ANY;
-     	serv_addr.sin_port = htons(portno);
-     	if (bind(sockfd, (struct sockaddr *) &serv_addr,
-        	sizeof(serv_addr)) < 0) 
-        	error("ERROR on binding");
-     	listen(sockfd,5);
-     	clilen = sizeof(cli_addr);
-	
-
-
-     /* receive uuid */
-     	newsockfd = accept(sockfd, 
-        		(struct sockaddr *) &cli_addr, &clilen);
-     	if (newsockfd < 0) 
-        	error("ERROR on accept");
-     	bzero(uuid_char, 33);
-     	if(read(newsockfd,uuid_char,33)<0)
-		error("Error: Failed reading uuid from socket");
-     	printf("uuid is: %s\n", uuid_char);     
-
-     	while (1) {
-        	newsockfd = accept(sockfd, 
-               				(struct sockaddr *) &cli_addr, &clilen);
-        	if (newsockfd < 0) 
-        		error("ERROR on accept");
-        	pid = fork();
-        	if (pid < 0)
-        		error("ERROR on fork");
-        	if (pid == 0)  {
-        		close(sockfd);
-        		dostuff(newsockfd);
-			close(newsockfd);
-        		exit(0);
-        	}
-        	else 
-			close(newsockfd);
-     	} /* end of while */
-     	close(sockfd);
-     	return 0; /* we never get here */
 }
 
-/******** DOSTUFF() *********************
- There is a separate instance of this function 
- for each connection.  It handles all communication
- once a connnection has been established.
- *****************************************/
+void check_dir(const char *path)
+{
+	DIR * dir;
+	dir = opendir(path);
+	if(!dir){
+		if(mkdir(path, 0777) == -1)
+			error("Error: Failed to create directory");
+	}
+	else
+		closedir(dir);
+}
+	
 void dostuff (int sock)
 {
 	FILE *output = fopen("raw.txt", "a+");
@@ -106,6 +51,6 @@ void dostuff (int sock)
 		n = read(sock,buffer,255);
 		if(n<0) 
 			error("Error: Failed reading from socket in while loop");
-	}      
+	} 
 	fclose(output);
 }
