e5eb8557e5a0fa1085c29440c4b66f139a644073
Hao (Dino) Liu <L-Dinosaur@github.com>
2016-08-10 16:42:39 -0400

    cleaning up

--- Makefile
+++
@@ -1,2 +0,0 @@
-duetmake: main.c duettel.c duettel.h
-	gcc -g -o duettel main.c duettel.c duettel.h -lduet -lpthread -luuid
--- run.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-sudo modprobe duet
-sudo duet status start
-sudo ./duet_collect -f 20 -d 200 -o -e -g -p /var
